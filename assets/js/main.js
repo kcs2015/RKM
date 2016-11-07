29d080ce46d222f6105cbb70f6d1bf5ddd4c7984
kcs2015 <knightclientsolutions@gmail.com>
2016-11-07 13:10:31 -0500

    Disabled parallax made css backup

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ $(function () {
     /* Parallax background effect*/
   $('section[data-type="background"]').each(function(){
       var $bgobj = $(this); // assigning the object
-      console.log('object ',$bgobj);
+      //console.log('object ',$bgobj);
       $(window).scroll(function(){
           /* Get current xPos of background*/
           var backgroundPos = $($bgobj).css('backgroundPosition').split(" ");
@@ -14,14 +14,26 @@ $(function () {
             var xPos = backgroundPos[0],
                 yPos = backgroundPos[1];
           
+         /* console.log('default yPos:' + yPos );
+          console.log('scrolltop:' + $window.scrollTop() );*/
+          
+          if($window.scrollTop() == 0){
+              
+          } 
+          {
+              
+                        
           /* scrool the background at var speed
           
           the yPos is  a negative value because we're scrolling it UP!
           */
-          
+          /* console.log('old yPos:' + yPos );*/
           var yPos = -($window.scrollTop() / $bgobj.data('speed'));
           
-          
+          /* console.log('new yPos:' + yPos );*/
+              
+          }
+
           
           // Put together our final background position
           var coords =  xPos + ' ' + yPos + 'px';
