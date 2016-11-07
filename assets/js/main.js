803737ae124a98f886a15329771e62e96104eacd
kcs2015 <knightclientsolutions@gmail.com>
2016-11-06 23:52:50 -0500

    Resolved responsive issues
    Fixed page cover row
    added padding to page cover
    Fixed html markup for section
    Fixed parallax jump issue
    started page cover mobile responsive queries left off @ 992px

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,15 @@ $(function () {
     /* Parallax background effect*/
   $('section[data-type="background"]').each(function(){
       var $bgobj = $(this); // assigning the object
-      
+      console.log('object ',$bgobj);
       $(window).scroll(function(){
+          /* Get current xPos of background*/
+          var backgroundPos = $($bgobj).css('backgroundPosition').split(" ");
+            //now contains an array like ["0%", "50px"]
+
+            var xPos = backgroundPos[0],
+                yPos = backgroundPos[1];
+          
           /* scrool the background at var speed
           
           the yPos is  a negative value because we're scrolling it UP!
@@ -14,8 +21,10 @@ $(function () {
           
           var yPos = -($window.scrollTop() / $bgobj.data('speed'));
           
+          
+          
           // Put together our final background position
-          var coords =  '50% ' + yPos + 'px';
+          var coords =  xPos + ' ' + yPos + 'px';
           // Move the background
           $bgobj.css({backgroundPosition: coords})
       }); // end window scrool
